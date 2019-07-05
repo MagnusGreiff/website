ea6ec9728c0a3f642b64fc5d9035a35b9e20a3a9
Magnus Greiff <mangegreiff@gmail.com>
2019-07-05 21:26:51 +0200

    Add basic navigation ability & blank pages for home, cv, links & stuff.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CvComponent } from './cv/cv.component';
+import { LinksComponent } from './links/links.component';
+import { StuffComponent } from './stuff/stuff.component';
+import { RouterModule } from '@angular/router';
+
+import routes from './router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CvComponent,
+    LinksComponent,
+    StuffComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/router.ts
@@ -0,0 +1,25 @@
+import { StuffComponent } from './stuff/stuff.component';
+import { LinksComponent } from './links/links.component';
+import { CvComponent } from './cv/cv.component';
+import { HomeComponent } from './home/home.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'cv',
+        component: CvComponent
+    },
+    {
+        path: 'links',
+        component: LinksComponent
+    },
+    {
+        path: 'stuff',
+        component: StuffComponent
+    }
+]
+export default routes;
